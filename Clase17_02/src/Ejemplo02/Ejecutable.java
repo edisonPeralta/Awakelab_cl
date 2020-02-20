b435cc690f979f753d7d3b66e43b5bee40f53446
Edison Peralta <ep.moto.taxi@gmail.com>
2020-02-19 21:46:28 -0300

    19_02

--- Clase17_02/src/Ejemplo02/Ejecutable.java
+++ Clase17_02/src/Ejemplo02/Ejecutable.java
@@ -10,62 +10,59 @@ public class Ejecutable {
 		
 		Producto producto[]= new Producto[10];
 		
-	/*	Perecedero producto[]= new Perecedero[5];*/
-		
 		for(int i=0;i<5;i++) {
 				
 			producto[i]=new Perecedero();
 			
 			System.out.println("Agregue Nombre de "+(i+1)+"° Producto Perecedero");
 			producto[i].setNombre(scan.next());
-			System.out.println("Agregue Precio de "+(i+1)+"° Producto");
+			System.out.println("Agregue Precio de "+(i+1)+"° Producto Perecedero");
 			producto[i].setPrecio(scan.nextInt());
 			System.out.println("Agregue Cantidad de Productos");
 			producto[i].setCant(scan.nextInt());
 			System.out.println("Agregue Días para caducar");
-			producto[i].setDias_caduca(scan.nextInt());
+			((Perecedero)producto[i]).setDias_caduca(scan.nextInt());
 			
         }
 		
-	/*	NoPerecedero producto2[]= new NoPerecedero[5];
+		for(int i=5;i<10;i++) {
+			
+			producto[i]=new NoPerecedero();
+			
+			System.out.println("Agregue Nombre de Producto no perecedero");
+			producto[i].setNombre(scan.next());
+			System.out.println("Agregue Precio de Producto no perecedero");
+			producto[i].setPrecio(scan.nextInt());
+			System.out.println("Agregue Cantidad");
+			producto[i].setCant(scan.nextInt());
+			System.out.println("Agregue Tipo");
+			((NoPerecedero)producto[i]).setTipo(scan.next());
+			
+        }
 		
-		for(int i=0;i<5;i++) {
-			
-			producto2[i]=new NoPerecedero();
-			
-			System.out.println("Agregue Nombre de "+(i+1)+"° Producto no perecedero");
-			producto2[i].setNombre(scan.next());
-			System.out.println("Agregue Precio de "+(i+1)+"° Producto");
-			producto2[i].setPrecio(scan.nextInt());
-			System.out.println("Agregue Cantidad de Productos");
-			producto2[i].setCant(scan.nextInt());
-			System.out.println("Agregue Tipo de producto");
-			producto2[i].setTipo(scan.next());
-			
-        } */
 		
-		int contador=0;
 		
 		for(int i=0;i<10;i++) {
 			
 			if (i<=4) {
+				
 				System.out.println("=======================================");
 				System.out.println("Producto Perecedero N° "+(i+1)+" :");
-				System.out.println("Nombre: "+producto[i].getNombre());
-				System.out.println("Cantidad: "+producto[i].getCant());
-				System.out.println("Días para Caducar: "+producto[i].getDias_caduca());
-				System.out.println("Total: "+producto[i].calcular());
+				System.out.println("Días para Caducar: "+((Perecedero)producto[i]).getDias_caduca());
+			
 			}
 			
 			if (i>4 & i<10) {
+				
 				System.out.println("=======================================");
-				System.out.println("Producto no Perecedero N° "+(i+1)+" :");
-				System.out.println("Nombre: "+producto2[contador].getNombre());
-				System.out.println("Cantidad: "+producto2[contador].getCant());
-				System.out.println("Tipo: "+producto2[contador].getTipo());
-				System.out.println("Total: "+producto2[contador].calcular());
-				contador++;
+				System.out.println("Producto No Perecedero N° "+(i+1)+" :");
+				System.out.println("Tipo: "+((NoPerecedero)producto[i]).getTipo());
+			
 			}
+				
+				System.out.println("Nombre: "+producto[i].getNombre());
+				System.out.println("Cantidad: "+producto[i].getCant());
+				System.out.println("Total: "+producto[i].calcular());				
 			
 		}
 	
