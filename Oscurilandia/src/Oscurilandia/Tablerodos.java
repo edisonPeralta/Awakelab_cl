64b13418984fc56c624524dc59bbe14706f73828
Edison Peralta <ep.moto.taxi@gmail.com>
2020-02-26 22:22:21 -0300

    26_02

--- Oscurilandia/src/Oscurilandia/Tablerodos.java
+++ Oscurilandia/src/Oscurilandia/Tablerodos.java
@@ -38,11 +38,13 @@ public class Tablerodos {
 					+ getClass() + ", hashCode()=" + hashCode() + ", toString()=" + super.toString() + "]";
 		}
 		
-		public int crearCarro() {
-			return 0;
+		public void crearCarro() {
+//			Carro objCarro[] = new Carro[18];
 		}
 		
-	public void mostrarTablero(int largo, int ancho){
+	public int mostrarTablero(int largo, int ancho){
+		
+		int contador = 0;
 		
 		int[][] tableroM =new int [16][18];
 		for(int i = 0; i<16;i++) 
@@ -59,92 +61,35 @@ public class Tablerodos {
 		while(indice<18) {
 			
 			int i;
-		//	for (i=0;i<10;i++) {
-			/*	int cantOcup= (int)(Math.random()*5);
+			for (i=0;i<10;i++) {
+				int cantOcup= (int)(Math.random()*5);
 				int annoIngreso= (int)(Math.random()*5+2014);
-				int ubicX= (int)(Math.random()*15);
-				int ubicY= (int)(Math.random()*15);
+				int ubicX= (int)(Math.random()*14+1);
+				int ubicY= (int)(Math.random()*14+1);
 				int nivelArmad= (int)(Math.random()*5);
 				int cop= 1;
 				objCarro[i]= new Trupalla (cantOcup, "Año "+String.valueOf(annoIngreso), ubicX, ubicY, nivelArmad, "Cop "+String.valueOf(cop));
-				cop++;*/
-			int ubicX= 1;
-			int ubicY= 1;
-			objCarro[i]= new Trupalla (2, "Año "+String.valueOf(2014), ubicX, ubicY, 3, "Cop "+String.valueOf(2));
-				if(tableroM[ubicX][ubicY]==0) {
-					tableroM[ubicX][ubicY]=2;
+				cop++;
+				if(tableroM[ubicX][ubicY+1]==0) {
+					tableroM[ubicX][ubicY+1]=2;
 					indice++;
-				}//else {
-			//		i--;
-			//	}
-			}
-			
-			int i;
-			int p;
-			for (i=1;i<16;i++) {			
-				   
-				if ((i==1) && (i<16)){				
-					System.out.println("     1   2   3   4   5   6   7   8   9   10  11  12  13  14  15");    				
+				}else {
+					i--;
 				}
-				
-				System.out.println(" ______________________________________________________________");	
-				
-				for (p=1;p<18;p++) {	
-					
-					if (tableroM[i][p]==2) {
-						
-						if (p==18) {				
-							System.out.print("| C |");					
-						}else if((p==1) && (i<10)){						
-							System.out.print(" "+i);
-						}else if((p==1) && (i>9 && i<16)){
-							System.out.print(i);
-						}else {						
-							System.out.print(" | C");
-						}					
-							
-					}else
-						
-					if (tableroM[i][p]==1) {
-						
-						if (p==18) {				
-							System.out.print("| H |");					
-						}else if((p==1) && (i<10)){						
-							System.out.print(" "+i);
-						}else if((p==1) && (i>9 && i<16)){
-							System.out.print(i);
-						}else {						
-							System.out.print(" | H");
-						}					
-						
-					}else
-						
-					if (p==18) {				
-						System.out.print("|   |");					
-					}else if((p==1) && (i<10)){						
-						System.out.print(" "+i);
-					}else if((p==1) && (i>9 && i<16)){
-						System.out.print(i);
-					}else {						
-						System.out.print(" |  ");
-					}
-							
-				}
-					
-				System.out.println("");
 			}
 		
-			/*for (i=10;i<15;i++) {
+			for (i=10;i<15;i++) {
 				int cantOcup= (int)(Math.random()*5);
 				int annoIngreso= (int)(Math.random()*5+2014);
-				int ubicX= (int)(Math.random()*15);
-				int ubicY= (int)(Math.random()*15);
+				int ubicX= (int)(Math.random()*14+1);
+				int ubicY= (int)(Math.random()*13+1);
 				int alcTiro= (int)(Math.random()*5);
 				int cop= 1;
 				objCarro[i]= new Caguano (cantOcup, "Año "+String.valueOf(annoIngreso), ubicX, ubicY, alcTiro, "Color "+String.valueOf(cop));
 				cop++;
-				if(tableroM[ubicX+1][ubicY+3]==0 && tableroM[ubicX+1][(ubicY+3)+1]==0) {
-					tableroM[ubicX+1][ubicY+3]=3;
+				if(tableroM[ubicX][ubicY+1]==0 && tableroM[ubicX][(ubicY+1)+1]==0) {
+					tableroM[ubicX][ubicY+1]=3;
+					tableroM[ubicX][(ubicY+1)+1]=3;
 					indice++;
 				}else {
 					i--;
@@ -154,23 +99,94 @@ public class Tablerodos {
 			for (i=15;i<18;i++) {
 				int cantOcup= (int)(Math.random()*5);
 				int annoIngreso= (int)(Math.random()*5+2014);
-				int ubicX= (int)(Math.random()*15);
-				int ubicY= (int)(Math.random()*15);
+				int ubicX= (int)(Math.random()*12+1);
+				int ubicY= (int)(Math.random()*14+1);
 				int anno= (int)(Math.random()*10+2000);
 				int cop= 1;
 				objCarro[i]= new Kromi (cantOcup, "Año "+String.valueOf(annoIngreso), ubicX, ubicY, anno, String.valueOf(cop));
 				cop++;
-				if(tableroM[ubicX+1][ubicY+3]==0) {
-					tableroM[ubicX+1][ubicY+3]=4;
+				if(tableroM[ubicX][ubicY+1]==0 && tableroM[ubicX+1][ubicY+1]==0 && tableroM[ubicX+2][ubicY+1]==0) {
+					tableroM[ubicX][ubicY+1]=4;
+					tableroM[ubicX+1][ubicY+1]=4;
+					tableroM[ubicX+2][ubicY+1]=4;
 					indice++;
 				}else {
 					i--;
 				}
-			}*/
+			}
+		}
+		
+/*		
+		int i;
+		int p;
+		for (i=1;i<16;i++) {			
+			   
+			if ((i==1) && (i<16)){				
+				System.out.println("     1   2   3   4   5   6   7   8   9   10  11  12  13  14  15");    				
+			}
 			
+			System.out.println(" ______________________________________________________________");	
+			
+			for (p=1;p<18;p++) {	
+				
+				if (tableroM[i][p]==2) {
+					
+					if (p==18) {				
+						System.out.print("| T |");					
+					}else if((p==1) && (i<10)){						
+						System.out.print(" "+i);
+					}else if((p==1) && (i>9 && i<16)){
+						System.out.print(i);
+					}else {						
+						System.out.print(" | T");
+					}					
+					contador=2;
+				}else
+					
+				if (tableroM[i][p]==3) {
+						
+					if (p==18) {				
+						System.out.print("| C |");					
+					}else if((p==1) && (i<10)){						
+						System.out.print(" "+i);
+					}else if((p==1) && (i>9 && i<16)){
+						System.out.print(i);
+					}else {						
+						System.out.print(" | C");
+					}					
+					contador=3;	
+				}else
+					
+				if (tableroM[i][p]==4) {
+						
+					if (p==18) {				
+						System.out.print("| K |");					
+					}else if((p==1) && (i<10)){						
+						System.out.print(" "+i);
+					}else if((p==1) && (i>9 && i<16)){
+						System.out.print(i);
+					}else {						
+						System.out.print(" | K");
+					}					
+					contador=4;			
+				}else
+					
+				if (p==18) {				
+					System.out.print("|   |");					
+				}else if((p==1) && (i<10)){						
+					System.out.print(" "+i);
+				}else if((p==1) && (i>9 && i<16)){
+					System.out.print(i);
+				}else {						
+					System.out.print(" |  ");
+				}
+						
+			}
+				
+			System.out.println("");
 		}
 			
-/*
+
  * 		int largo=16;
 //		int ancho=18;
 		int i;
@@ -206,13 +222,25 @@ public class Tablerodos {
 			System.out.println(" ______________________________________________________________"); 
 	} 
 */
+		return contador;
 	
 	}	  
 	
 	public void lanzarHuevo() {
 		
+		
+		int[][] aux =new int [16][18];
+		for(int i = 0; i<16;i++) 
+		{
+			for(int j=0;j<18;j++)
+			{
+				aux[i][j]=mostrarTablero(i,j);
+				
+			}
+		}
+		
 		Scanner read = new Scanner(System.in);
-		int a=0;
+		int total=0;
 		int numeroA;
 		int numeroB;
 		int z;
@@ -226,8 +254,6 @@ public class Tablerodos {
 			}
 		}
 		
-//		ArrayList huevo= new ArrayList();
-		
 		do {
 			
 			int i;
@@ -235,8 +261,7 @@ public class Tablerodos {
 			for (i=1;i<16;i++) {			
 				   
 				if ((i==1) && (i<16)){				
-					System.out.println					
-					("     1   2   3   4   5   6   7   8   9   10  11  12  13  14  15");    				
+					System.out.println("     1   2   3   4   5   6   7   8   9   10  11  12  13  14  15");    				
 				}
 				
 				System.out.println(" ______________________________________________________________");				
@@ -244,6 +269,7 @@ public class Tablerodos {
 				for (p=1;p<18;p++) {			
 						
 					if (tablero[i][p]==1) {
+						
 						if (p==18) {				
 							System.out.print("| H |");					
 						}else if((p==1) && (i<10)){						
@@ -253,6 +279,7 @@ public class Tablerodos {
 						}else {						
 							System.out.print(" | H");
 						}
+						
 					}else if (p==18) {				
 						System.out.print("|   |");					
 					}else if((p==1) && (i<10)){						
@@ -262,7 +289,7 @@ public class Tablerodos {
 					}else {						
 						System.out.print(" |  ");
 					}
-							
+					
 				}
 					
 				System.out.println("");
@@ -270,6 +297,11 @@ public class Tablerodos {
 			
 			System.out.println(" ______________________________________________________________"); 
 			
+			System.out.println(" ");
+			System.out.println("==============================");
+			System.out.println("Su Puntaje actual es: "+total);
+			System.out.println("==============================");
+			System.out.println(" ");
 			System.out.println("Usted esta jugando 'Oscurilandia' para salir digite 0 en horizontal y vertical");
 			System.out.println(" ");
 			System.out.println("Elija un numero orizontal");
@@ -280,9 +312,11 @@ public class Tablerodos {
 			z = read.nextInt();
 			numeroB = z;
 			
+//			static ArrayList <Huevo> huevos= new ArrayList();
+			
 			if (numeroA!=0 | numeroB!=0)	{	
 				
-//				huevo.add(numeroB, numeroA+1);
+//				huevos.add();
 			
 				tablero[numeroB][numeroA+1]=1; 
 				System.out.println("Exito!!");
@@ -290,9 +324,51 @@ public class Tablerodos {
 			
 			}
 			
+			if (aux[numeroB][numeroA+1]==2) { //Sumamos Valor de las "TRUPALLAS" =1PTS
+				
+				total=total+1;
+				aux[numeroB][numeroA+1]=7;
+				
+			}
+			
+			if (aux[numeroB][numeroA+1]==3) { //Sumamos Valor de las "CAGUANOS" =2PTS
+				
+				if	(aux[numeroB][numeroA+1]+1==2) { //Sumamos al Valor 7PTS en caso que se destruya completo el "CAGUANO"
+					
+					total=total+7;
+					
+				}else if(aux[numeroB][numeroA]==5){ //Sumamos al Valor 7PTS en caso que se destruya completo el "CAGUANO"
+					
+					total=total+7;
+					aux[numeroB][numeroA+1]=5;
+				}
+				
+				total=total+2;
+				
+			}
+			
+			if (aux[numeroB][numeroA+1]==4) { //Sumamos Valor de las "KROMIS" =3PTS
+				
+				if (aux[numeroB+1][numeroA+1]==6 && aux[numeroB+2][numeroA+1]==6 | aux[numeroB+1][numeroA+1]==6 && aux[numeroB-1][numeroA+1]==6) { //Sumamos al Valor 10PTS en caso que se destruya completo el "KROMI"
+					
+					total=total+10;
+					
+				}else if(aux[numeroB-1][numeroA]==6 && aux[numeroB-2][numeroA+1]==6){ //Sumamos al Valor 10PTS en caso que se destruya completo el "KROMI"
+					
+					total=total+10;
+					
+				}
+				
+				total=total+3;
+				aux[numeroB][numeroA+1]=6;
+								
+			}
+			
 		}while (numeroA+numeroB != 0);
 			
 		System.out.println("Good Bye...");	
+		
+		
 //				for (int i = 0; i < z; i++){	
 //					    if(i==0) {
 //					    	System.out.println("tu huevo se estrelló en el suelo :(");
