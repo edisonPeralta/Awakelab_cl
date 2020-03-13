3b0812536272b41b06b145bd9d3279ddf87d8d3b
Edison Peralta <ep.moto.taxi@gmail.com>
2020-03-12 21:05:55 -0300

    12/03

---
+++ Prueba 12_03/Prueba.sql
@@ -0,0 +1,39 @@
+--1
+INSERT INTO preguntas(id_preguntas, enunciado, puntaje, test_id_test)
+VALUES(1.1, '', 10, 001);
+
+--2
+INSERT INTO preguntas(id_preguntas, enunciado, puntaje, test_id_test)
+VALUES(1.2, '', 10, 001);
+
+--3
+INSERT INTO preguntas(id_preguntas, enunciado, puntaje, test_id_test)
+VALUES(1.3, '', 10, 001);
+
+--4
+INSERT INTO preguntas(id_preguntas, enunciado, puntaje, test_id_test)
+VALUES(1.4, '', 10, 001);
+
+--5
+INSERT INTO preguntas(id_preguntas, enunciado, puntaje, test_id_test)
+VALUES(1.5, '', 10, 001);
+
+--6
+INSERT INTO preguntas(id_preguntas, enunciado, puntaje, test_id_test)
+VALUES(1.6, '', 10, 001);
+
+--7
+INSERT INTO preguntas(id_preguntas, enunciado, puntaje, test_id_test)
+VALUES(1.7, '', 10, 001);
+
+--8
+INSERT INTO preguntas(id_preguntas, enunciado, puntaje, test_id_test)
+VALUES(1.8, '', 10, 001);
+
+--9
+INSERT INTO preguntas(id_preguntas, enunciado, puntaje, test_id_test)
+VALUES(1.9, '', 10, 001);
+
+--10
+INSERT INTO preguntas(id_preguntas, enunciado, puntaje, test_id_test)
+VALUES(1.10, '', 10, 001);
---
+++ Prueba 12_03/Prueba~1.sql
@@ -0,0 +1,9 @@
+--Insertar datos del test.
+--1
+INSERT INTO test(id_test, nombre, descripcion, programa, unidad, autor, fecha_creacion, curso_id_curso)
+VALUES(001, 'BBDD', 'Evaluación de base de datos.', 'FullStack Java Trainnie.', 'Talento Digital', 'Awakelab', '03/03/2020', 025);
+
+--2
+INSERT INTO test(id_test, nombre, descripcion, programa, unidad, autor, fecha_creacion, curso_id_curso)
+VALUES(002, 'PSeInt', 'Evaluación uso de PSeInt.', 'Programación back end', 'Talento Digital', 'Awakelab', '08/04/2020', 026);
+
